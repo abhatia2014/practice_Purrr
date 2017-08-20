cccc9bee1769a4c173e26801fa8df392606f5356
Aankur Bhatia <aank2007@gmail.com>
2017-08-20 19:59:52 -0400

    save progress

--- practice_purr.R
+++ practice_purr.R
@@ -8,3 +8,39 @@ library(repurrrsive)
 
 head(got_chars[1:3])
 
+# using map function to the list
+# extracting the top three names
+str(got_chars)
+
+map(got_chars[1:3],"name")
+
+
+# sapply vs purrr ---------------------------------------------------------
+
+# use map_chr to get an atomic vector and not list
+
+map_chr(got_chars[1:5],"culture")
+
+
+# map_df to convert to data frame -----------------------------------------
+
+map_df(got_chars[23:25],`[`,c('name','playedBy','culture'))
+
+
+# however, the method may not work if data types of different types are extracted
+
+map_df(got_chars[23:25],`[`,c('name','id'))
+
+#alternatively, one can use the tibble feature
+
+tibble::tibble(
+  name=map_chr(got_chars[1:15],"name"),
+  id=map_int(got_chars[1:15],"id")
+  
+  
+)
+
+
+# Concatenate two strings using map ---------------------------------------
+
+
