97eb0d158f50f78a8abacafe79b4bdd97d7c29eb
Aankur Bhatia <aank2007@gmail.com>
2017-08-20 18:14:02 -0400

    first commit- loading packages

---
+++ practice_purr.R
@@ -0,0 +1,10 @@
+# link https://jennybc.github.io/purrr-tutorial/bk01_base-functions.html
+
+library(purrr)
+
+library(githubinstall)
+devtools::install_github("jennybc/repurrrsive")
+library(repurrrsive)
+
+head(got_chars[1:3])
+
